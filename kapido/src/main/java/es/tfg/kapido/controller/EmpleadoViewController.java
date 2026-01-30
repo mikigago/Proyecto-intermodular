baddd3895e6ae6b660859a83a155482df7a71ad9
mikigago <miguelgago1995@gmail.com>
2026-01-30 16:00:21 +0100

    Empezando de nuevo

--- kapido/src/main/java/es/tfg/kapido/controller/EmpleadoViewController.java
+++ kapido/src/main/java/es/tfg/kapido/controller/EmpleadoViewController.java
@@ -9,6 +9,7 @@ import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+//CODE REVIEW 
 
 @Controller
 @RequestMapping("/empleados")
